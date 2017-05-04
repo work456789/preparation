46b6cf07e7aa7a3b2f352edc6c82a062ed11488f
work456789 <work456789@gmail.com>
2017-05-04 07:31:13 +0300

    Restructured code.

--- com/src/preparation/Test.java
+++
@@ -1,8 +0,0 @@
-package com.src;
-
-public class Test {
-    public static void main(String[] args) {
-        LinkedListCustom linkedListCustom = new LinkedListCustom();
-        System.out.println(linkedListCustom.toString());
-    }
-}
