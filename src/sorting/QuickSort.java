7d887d711202c88873ba9e4dfe6e637a121174b6
work456789 <work456789@gmail.com>
2017-05-05 08:59:18 +0300

    quicksort

--- src/sorting/QuickSort.java
+++ src/sorting/QuickSort.java
@@ -1,5 +1,7 @@
 package sorting;
 
+import java.util.Arrays;
+
 //O(nlogn) expected
 //O(n^2) worst case
 public class QuickSort {
@@ -12,26 +14,48 @@ public class QuickSort {
         partition(0, input.length - 1);
     }
 
-    static void partition(int left, int right) {
+    static void partition(int low, int high) {
+        int i = low, j = high;
+        int pivot = numbers[low + (high - low)/2];
 
+        while(i <= j) {
+            while(numbers[i] < pivot) {
+                i++;
+            }
+
+            while(numbers[j] > pivot) {
+                j--;
+            }
+
+            if (i <= j) {
+                exchange(i, j);
+                i++;
+                j--;
+            }
+        }
+
+        if (low < j) {
+            partition(low, j);
+        }
+
+        if(i < high){
+            partition(i, high);
+        }
+    }
+
+    private static void exchange(int i, int j) {
+        int temp = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = temp;
     }
 
     public static void main(String args[]){
-//        numbers = new int[]{33, 21, 45, 64, 55, 34, 11, 8, 3, 5, 1};
-//        System.out.println("Before Sorting : ");
-//        System.out.println(Arrays.toString(numbers));
-//        QuickSort quickSort = new QuickSort();
-//        quickSort(numbers);
-//        System.out.println("==================");
-//        System.out.println("After Sorting : ");
-//        System.out.println(Arrays.toString(numbers));
-        Integer a = 50;
-        Integer b = 50;
-        a.equals(b);
-       // System.out.println(a == b);
-
-        Long c = 100L;
-        Long d = 100L;
-        System.out.println(c == d);
+        numbers = new int[]{33, 21, 45, 64, 55, 34, 11, 8, 3, 5, 1};
+        System.out.println("Before Sorting : ");
+        System.out.println(Arrays.toString(numbers));
+        quickSort(numbers);
+        System.out.println("==================");
+        System.out.println("After Sorting : ");
+        System.out.println(Arrays.toString(numbers));
     }
 }
