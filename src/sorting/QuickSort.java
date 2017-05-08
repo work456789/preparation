07f9ebbf68897e77c02617a553f68138142de17d
work456789 <work456789@gmail.com>
2017-05-08 10:07:04 +0300

    Refactored some code.

--- src/sorting/QuickSort.java
+++ src/sorting/QuickSort.java
@@ -4,56 +4,45 @@ import java.util.Arrays;
 
 //O(nlogn) expected
 //O(n^2) worst case
+//O(1) space complexity -> this is inplace sorting algorithm
 public class QuickSort {
     private static int numbers[];
 
-    static void quickSort(int [] input) {
+    static void quickSort(int [] input, int start, int end) {
         if (input == null || input.length == 0) {
             return;
         }
-        partition(0, input.length - 1);
-    }
-
-    static void partition(int low, int high) {
-        int i = low, j = high;
-        int pivot = numbers[low + (high - low)/2];
-
-        while(i <= j) {
-            while(numbers[i] < pivot) {
-                i++;
-            }
-
-            while(numbers[j] > pivot) {
-                j--;
-            }
-
-            if (i <= j) {
-                exchange(i, j);
-                i++;
-                j--;
-            }
-        }
-
-        if (low < j) {
-            partition(low, j);
-        }
-
-        if(i < high){
-            partition(i, high);
+        if (start < end) {
+            int partitionIndex = partition(input, start, end);
+            quickSort(input, start, partitionIndex-1);
+            quickSort(input, partitionIndex+1, end);
         }
     }
 
-    private static void exchange(int i, int j) {
+    static int partition(int [] input, int start, int end) {
+        int pivot = input[start];
+        int pivotIndex = end;
+        for (int i = end; i > start; i--) {
+            if (input[i] >= pivot) {
+                swap(i, pivotIndex);
+                pivotIndex--;
+            }
+        }
+        swap(pivotIndex, start);
+        return pivotIndex;
+    }
+
+    private static void swap(int i, int j) {
         int temp = numbers[i];
         numbers[i] = numbers[j];
         numbers[j] = temp;
     }
 
     public static void main(String args[]){
-        numbers = new int[]{33, 21, 45, 64, 55, 34, 11, 8, 3, 5, 1};
+        numbers = new int[]{33, 21, 45, 64, 55, 34, 11, 3, 5, 1};
         System.out.println("Before Sorting : ");
         System.out.println(Arrays.toString(numbers));
-        quickSort(numbers);
+        quickSort(numbers, 0, numbers.length-1);
         System.out.println("==================");
         System.out.println("After Sorting : ");
         System.out.println(Arrays.toString(numbers));
